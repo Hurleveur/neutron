6379ddaa3df314db7ddc590d04c1bb4872407b09
Dark-Peace <hanssens.dja@gmail.com>
2023-12-28 11:35:39 +0100

    planet shader finally fixed

--- src/planet.fs
+++ src/planet.fs
@@ -1,9 +1,9 @@
-varying vec4 texCoords;
+varying vec3 TexCoords;
 uniform sampler2D mytexture;
 
 void main(void) {
-    vec2 longitudeLatitude = vec2((atan(texCoords.y, texCoords.x) / 3.1415926 + 1.0) * 0.5,
-                                  (asin(texCoords.z) / 3.1415926 + 0.5));
+    vec2 longitudeLatitude = vec2((atan(TexCoords.y, TexCoords.x) / 3.1415926 + 1.0) * 0.5,
+                                  (asin(TexCoords.z) / 3.1415926 + 0.5));
 
     gl_FragColor = texture2D(mytexture, longitudeLatitude);
 }
